6918c5d69c90032709036076b70b82ce62959268
manuhutu344 <nandoperdos2@gmail.com>
2024-04-23 19:49:35 +0900

    Membuat quismecard dan history card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import HistoryCard from '@/components/dashboard/HistoryCard'
+import QuisMeCard from '@/components/dashboard/QuisMeCard'
 import { getAuthSession } from '@/lib/nextauth'
 import { redirect } from 'next/navigation'
 import React from 'react'
+
+export const metadata={
+  title: "Dashboard | Quis Saja"
+}
 
 async function page() {
     const session = await getAuthSession()
@@ -8,7 +14,18 @@ async function page() {
         return redirect('/')
     }
   return (
-    <div>page</div>
+    <main className="p-8 mx-auto max-x-7xl">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tighter">Dashboard</h2>
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+          <QuisMeCard />
+          <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/dashboard/QuisMeCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardHeader } from '../ui/card'
+
+function QuisMeCard() {
+  return (
+    <Card className="hover:cursor-pointer hover:opacity-75">
+        <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+
+        </CardHeader>
+    </Card>
+  )
+}
+
+export default QuisMeCard

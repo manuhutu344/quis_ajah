095c777e6bf10d8ca0736d8166783aa975ae09a4
manuhutu344 <nandoperdos2@gmail.com>
2024-04-24 20:26:24 +0900

    Membuat history dan quiscard

--- src/components/dashboard/HistoryCard.tsx
+++ src/components/dashboard/HistoryCard.tsx
@@ -1,8 +1,28 @@
+"use client"
+
 import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { History } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 function HistoryCard() {
+  const router = useRouter()
   return (
-    <div>HistoryCard</div>
+    <Card onClick={()=>{
+      router.push("/history")
+    }} className="hover:cursor-pointer hover:opacity-75">
+      <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+          <CardTitle className="text-2xl font-bold">
+            History
+          </CardTitle>
+          <History size={28} strokeWidth={2.5} />
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm text-muted-foreground">
+            Lihat Postingan Quis
+        </p>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/components/dashboard/QuisMeCard.tsx
+++ src/components/dashboard/QuisMeCard.tsx
@@ -1,12 +1,27 @@
+"use client"
+
 import React from 'react'
-import { Card, CardHeader } from '../ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { BrainCircuit } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 function QuisMeCard() {
+  const router = useRouter()
   return (
-    <Card className="hover:cursor-pointer hover:opacity-75">
+    <Card onClick={()=>{
+      router.push("/quis")
+    }} className="hover:cursor-pointer hover:opacity-75">
         <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-
+          <CardTitle className="text-2xl font-bold">
+            Quis Saya !
+          </CardTitle>
+          <BrainCircuit size={28} strokeWidth={2.5} />
         </CardHeader>
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+              Tantang Diri Anda Dengan Kuis
+          </p>
+        </CardContent>
     </Card>
   )
 }

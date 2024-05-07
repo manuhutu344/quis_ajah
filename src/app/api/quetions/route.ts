fcf93af7e5d2ca4f157d0acfaff7b0718c7850af
manuhutu344 <nandoperdos2@gmail.com>
2024-05-07 19:51:38 +0900

    Membuat session untuk validasi serta membuat generet 3 option

--- src/app/api/quetions/route.ts
+++ src/app/api/quetions/route.ts
@@ -2,19 +2,45 @@ import { NextResponse } from "next/server";
 import { quizCreationSchema } from "@/schemas/form/quis";
 import { ZodError } from "zod";
 import { strict_output } from "@/lib/gpt";
+import { getAuthSession } from "@/lib/nextauth";
 
 export const POST = async (req:Request, res: Response)=>{
     try {
+        const session = await getAuthSession()
+        if(!session?.user){
+            return NextResponse.json(
+                {
+                    error: "Kamu harus login dulu"
+                },
+                {
+                    status: 401
+                }
+            )
+        }
         const body = await req.json()
         const {amount, topic, type} = quizCreationSchema.parse(body)
         let questions: any
         if(type === "open_ended"){
             questions = await strict_output(
                 "Anda dapat dibantu dengan Ai dari gpt untuk membantu anda dalam memakai aplikasi ini",
-                `Anda dapat mengeneret apapun di ${topic}`,
+                new Array(amount).fill(`Anda dapat mengeneret apapun di ${topic}`),
                 {
                     question: "question",
                     answer: "Jawaban dengan panjang maksimal 15 kata"
+                }
+            )
+        }else if (type === "mcq"){
+            questions = await strict_output(
+                'Anda dapat dibantu dengan AI untuk generate mcq dari pertanyaan ataupun jawaban, minimal pertannyan adalah 15 kata',
+                new Array(amount).fill(
+                    `Anda dapat mengeneret apapun di ${topic}`
+                ),
+                {
+                    question: 'question',
+                    answer: 'Jawaban dengan panjang maksimal 15 kata',
+                    option1: "Pertanyaan 1 dengan panjang maksimal 15 kata",
+                    option2: "Pertanyaan 2 dengan panjang maksimal 15 kata",
+                    option3: "Pertanyaan 3 dengan panjang maksimal 15 kata",
                 }
             )
         }
